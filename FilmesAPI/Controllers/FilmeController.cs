a5fa8fb65dbd7d394eb5c4e7968c8b325460d8f5
Lucas <lucasamarale@hotmail.com>
2023-06-27 22:54:53 -0300

    PUT e PATCH para atualizar filmes

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -2,6 +2,7 @@
 using FilmesAPI.Data;
 using FilmesAPI.Data.DTOs;
 using FilmesAPI.Models;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FilmesAPI.Controllers;
@@ -25,6 +26,7 @@ public class FilmeController : ControllerBase
         Filme filme = _mapper.Map<Filme>(filmeDto);
         _context.Filmes.Add(filme);
         _context.SaveChanges();
+
         return CreatedAtAction(
             nameof(RecuperaFilmePorId),
             new { id = filme.Id },
@@ -45,8 +47,49 @@ public class FilmeController : ControllerBase
     public IActionResult RecuperaFilmePorId(int id)
     {
         var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+
         if (filme == null)
             return NotFound();
+
+        return Ok(filme);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult AtualizaFilme(
+        int id, 
+        [FromBody] UpdateFilmeDto filmeDto
+        )
+    {
+        var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+
+        if (filme == null)
+            return NotFound();
+
+        _mapper.Map(filmeDto, filme);
+        _context.SaveChanges();
+        return Ok(filme);
+    }
+
+    [HttpPatch("{id}")]
+    public IActionResult AtualizaFilmeParcial(
+        int id, 
+        JsonPatchDocument<UpdateFilmeDto> patch
+        )
+    {
+        var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+
+        if (filme == null)
+            return NotFound();
+
+        var filmeParaAtualizar = _mapper.Map<UpdateFilmeDto>(filme);
+        patch.ApplyTo(filmeParaAtualizar, ModelState);
+        if(!TryValidateModel(filmeParaAtualizar))
+        {
+            return ValidationProblem(ModelState);
+        }
+
+        _mapper.Map(filmeParaAtualizar, filme);
+        _context.SaveChanges();
         return Ok(filme);
     }
 }

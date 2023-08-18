fb8e7e2e1805c4666cb7dce73041ba62e20e6d08
Lucas <lucasamarale@hotmail.com>
2023-08-18 17:03:59 -0300

    Aula 01 - Crescendo o projeto

---
+++ FilmesAPI/Profiles/CinemaProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using FilmesAPI.Data.DTOs;
+using FilmesAPI.Models;
+
+namespace FilmesAPI.Profiles
+{
+    public class CinemaProfile : Profile
+    {
+        public CinemaProfile()
+        {
+            CreateMap<CreateCinemaDto, Cinema>();
+            CreateMap<ReadCinemaDto, Cinema>();
+            CreateMap<UpdateCinemaDto, Cinema>();
+        }
+    }
+}
